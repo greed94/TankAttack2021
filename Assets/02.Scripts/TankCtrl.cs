428b7889e951dda893a55af455d23757f45d4b9f
greed94 <80263847+greed94@users.noreply.github.com>
2021-04-27 11:53:33 +0900

    오전수업끝

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityStandardAssets.Utility;
 
 public class TankCtrl : MonoBehaviour
 {
     private Transform tr;
     public float speed = 10.0f;
+    private PhotonView pv;
 
     void Start()
     {
         tr = GetComponent<Transform>();    
+        pv = GetComponent<PhotonView>();
+
+        if (pv.IsMine)
+        {
+            Camera.main.GetComponent<SmoothFollow>().target = tr.Find("CamPivot").transform;
+            GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -5.0f, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+
         GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -5.0f, 0);
     }
 
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
+        if (pv.IsMine)
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
 
-        tr.Translate(Vector3.forward * Time.deltaTime * speed * v);
-        tr.Rotate(Vector3.up * Time.deltaTime * 100.0f * h);
+            tr.Translate(Vector3.forward * Time.deltaTime * speed * v);
+            tr.Rotate(Vector3.up * Time.deltaTime * 100.0f * h);
+        }
     }
 }
