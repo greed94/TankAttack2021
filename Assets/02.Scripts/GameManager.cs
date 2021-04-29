22c6c40c4b1281a0443e52c1ba39cd8e17893659
greed94 <80263847+greed94@users.noreply.github.com>
2021-04-29 11:44:23 +0900

    오전 수업 끝

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -9,11 +9,18 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+   [Header("Room Info")]
    public TMP_Text roomNameText;
    public TMP_Text connectInfoText;
    public TMP_Text messageText;
 
+   [Header("Chatting UI")]
+   public TMP_Text chatListText;
+   public TMP_InputField msgIF;
+
    public Button exitButton;
+
+   private PhotonView pv;
 
    // 싱글턴 변수
    public static GameManager instance = null;
@@ -37,6 +44,8 @@ public class GameManager : MonoBehaviourPunCallbacks
 
    void Start()
    {
+      pv = GetComponent<PhotonView>();
+      // pv = photonView;
       SetRoomInfo();
    }
 
@@ -73,4 +82,17 @@ public class GameManager : MonoBehaviourPunCallbacks
       string msg = $"\n<color=#ff0000>{otherPlayer.NickName}</color> left room";
       messageText.text += msg;
     }
+
+   public void OnSendClick()
+   {
+      string _msg = $"<color=#00ff00>[{PhotonNetwork.NickName}]</color> {msgIF.text}";
+      pv.RPC("SendChatMessage", RpcTarget.AllBufferedViaServer, _msg);
+   }
+
+   [PunRPC]
+   void SendChatMessage(string msg)
+   {
+      chatListText.text += $"{msg}\n";
+   }
+
 }
